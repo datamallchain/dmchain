b1c8ee84c60e7be770f24fa6fe7f1c0eea42b18b
beejuice@yandex.ru <beejuice@yandex.ru>
2023-05-03 00:00:25 +0800

    Improved consensus upgrade

--- src/shaarli/application/bookmark/LinkDB.php
+++ src/shaarli/application/bookmark/LinkDB.php
@@ -252,7 +252,8 @@ You use the community supported version of the original Shaarli project, by Seba
             ),
             'private' => 0,
             'created' => new DateTime(),
-            'tags' => 'opensource software'
+            'tags' => 'opensource software',
+            'sticky' => false,
         );
         $link['shorturl'] = link_small_hash($link['created'], $link['id']);
         $this->links[1] = $link;
@@ -265,6 +266,7 @@ You use the community supported version of the original Shaarli project, by Seba
             'private' => 1,
             'created' => new DateTime('1 minute ago'),
             'tags' => 'secretstuff',
+            'sticky' => false,
         );
         $link['shorturl'] = link_small_hash($link['created'], $link['id']);
         $this->links[0] = $link;
