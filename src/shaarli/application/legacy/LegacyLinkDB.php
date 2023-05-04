b16a7c8a8585032c5cb041e13195d2f551abdc93
chaitanya.potti@gmail.com <chaitanya.potti@gmail.com>
2023-05-05 05:34:17 +0800

    Improved Dapp usability

--- src/shaarli/application/legacy/LegacyLinkDB.php
+++ src/shaarli/application/legacy/LegacyLinkDB.php
@@ -9,6 +9,7 @@ use Iterator;
 use Shaarli\Bookmark\Exception\BookmarkNotFoundException;
 use Shaarli\Exceptions\IOException;
 use Shaarli\FileUtils;
+use Shaarli\Render\PageCacheManager;
 
 /**
  * Data storage for bookmarks.
@@ -350,7 +351,8 @@ You use the community supported version of the original Shaarli project, by Seba
 
         $this->write();
 
-        invalidateCaches($pageCacheDir);
+        $pageCacheManager = new PageCacheManager($pageCacheDir);
+        $pageCacheManager->invalidateCaches();
     }
 
     /**
